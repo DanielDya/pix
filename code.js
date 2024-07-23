1daef1872c017f3a79098368841bd68f20ec0b41
João Daniel B Bernardino de lima <122681503+DanielDya@users.noreply.github.com>
2024-07-23 09:18:58 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,24 @@
+function esquivar(){
+    var botao = document.querySelector("#não");
+
+    botao.style.position = "absolute";
+    botao.style.top = mudarposicao(4,95)
+    botao.style.left = mudarposicao(4,95)
+}
+function mudarposicao(min,max){
+    let position = Math.floor(Math.random() * (max - min)+min)
+
+    return position + "%"
+}
+function respostasim(){
+    alert("(╯°□°）╯︵ ┻━┻")
+}
+
+function esquivarmobile(){
+    esquivar();
+    setTimeout(esquivarmobile,200)
+}
+
+if(window.innerWidth <= 450){
+esquivarmobile();
+}
